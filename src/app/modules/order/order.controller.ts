c863adff1d4417d4e641663a84598c1b9a252574
Mahmud Munna <smbmunna@gmail.com>
2024-05-23 14:17:16 +0600

    In Order creation validation applied

--- src/app/modules/order/order.controller.ts
+++ src/app/modules/order/order.controller.ts
@@ -6,16 +6,17 @@ const createOrder = async (req: Request, res: Response) => {
   try {
     const order = req.body;
     const result = await OrderServices.createOrderIntoDB(order);
-
-    res.status(200).json({
-      success: true,
-      message: "Order created successfully!",
-      data: result,
-    });
+    if (result) {
+      res.status(200).json({
+        success: true,
+        message: "Order created successfully!",
+        data: result,
+      });
+    }
   } catch (err) {
     res.status(500).json({
       success: false,
-      message: "Cannot create order!",
+      message: "Invalid Product.",
       error: err,
     });
   }
@@ -25,15 +26,6 @@ const createOrder = async (req: Request, res: Response) => {
 const getAllOrders = async (req: Request, res: Response) => {
   try {
     const email = req.query.email;
-    // if (email) {
-    //   const email = req.query.email as string;
-    //   const result = await OrderServices.getOrdersByEmailFromDB(email);
-    //   return res.status(200).json({
-    //     success: true,
-    //     message: "Orders fetched successfully for user email!",
-    //     data: result,
-    //   });
-    // }
     const result = await OrderServices.getAllOrdersFromDB(email as string);
     if (result.length > 0 && !email) {
       return res.status(200).json({
--- src/app/modules/order/order.services.ts
+++ src/app/modules/order/order.services.ts
@@ -1,10 +1,24 @@
+import { Types } from "mongoose";
+import { ProductModel } from "../product/product.model";
 import { TOrder } from "./order.interface";
 import { OrderModel } from "./order.model";
 
 //---------------create an order
 const createOrderIntoDB = async (order: TOrder) => {
-  const result = await OrderModel.create(order);
-  return result;
+  //check if product id is valid
+  const productIdObject = new Types.ObjectId(order.productId);
+
+  const foundProduct = await ProductModel.aggregate([
+    {
+      $match: { _id: productIdObject },
+    },
+  ]);
+  if (foundProduct) {
+    const result = await OrderModel.create(order);
+    return result;
+  } else {
+    return false;
+  }
 };
 
 //---------------get all orders
